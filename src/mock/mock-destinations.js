4a2fa17165601077f14b6ebd604145a49e99de8a
Slava Kochnev <sl.kochnev@mail.ru>
2026-01-20 12:55:57 +0500

    Добавляю моковые данные

---
+++ src/mock/mock-destinations.js
@@ -0,0 +1,39 @@
+const mockDestinations = [
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808c',
+    description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Chamonix',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building'
+      }
+    ]
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808s',
+    description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Le Brevent',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building'
+      }
+    ]
+  },
+  {
+    id: 'f4b62099-293f-4c3d-a702-94eec4a2808e',
+    description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Servoz',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building'
+      }
+    ]
+  }
+];
+
+const getRandomDestinations = () => mockDestinations;
+
+export {getRandomDestinations};
---
+++ src/mock/mock-offers.js
@@ -0,0 +1,37 @@
+const mockOffers = [
+  {
+    type: 'taxi',
+    'offers': [
+      {
+        id: 'f4b62099-293f-4c3d-a702-94eec4a2808c',
+        title: 'Upgrade to a business class',
+        price: 120
+      }
+    ]
+  },
+  {
+    type: 'taxi',
+    'offers': [
+      {
+        id: 'f4b62099-293f-4c3d-a702-94eec4a2808s',
+        title: 'Upgrade to a business class',
+        price: 120
+      }
+    ]
+  },
+  {
+    type: 'taxi',
+    'offers': [
+      {
+        id: 'f4b62099-293f-4c3d-a702-94eec4a2808e',
+        title: 'Upgrade to a business class',
+        price: 120
+      }
+    ]
+  }
+];
+
+const getRandomOffers = () => mockOffers;
+
+export {getRandomOffers};
+
